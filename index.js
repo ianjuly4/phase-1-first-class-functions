c4a4cf1fb192f72cc594a1a8b83207660e2cfb44
Ian July <ianjuly4@gmail.com>
2023-08-13 12:19:01 -0600

    first class functions

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(recieves){
+    console.log(recieves());
+}
+function returnsANamedFunction (){
+    return returnsANamedFunction;
+}
+function returnsAnAnonymousFunction(){
+    return function(){};
+}
